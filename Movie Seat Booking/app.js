62b61fb560c122910518c5106a2f9c22073f2616
PhilipGreene08 <54162361+PhilipGreene08@users.noreply.github.com>
2021-03-31 20:45:25 -0500

    Add files via upload

--- Movie Seat Booking/app.js
+++ Movie Seat Booking/app.js
@@ -7,13 +7,27 @@ const movieSelect = document.getElementById('movie')
 //+ turns the value from a string into a number instead of using parse
 let ticketPrice = +movieSelect.value
 
+//save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex)
+    localStorage.setItem('selectedMoviePrice', moviePrice)
+}
+
 //update total count
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected')
-    console.log(selectedSeats);
+
+    const seatsIndex = [...selectedSeats].map(function (seat) {
+        return [...seats].indexOf(seat)
+    })
+    //copy selected seats into array
+    //map through
+    //return new array of index
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex))
 
     const selectedSeatCount = selectedSeats.length
-    console.log(selectedSeatCount);
+
 
     count.innerText = selectedSeatCount
     total.innerText = selectedSeatCount * ticketPrice
@@ -23,6 +37,8 @@ function updateSelectedCount() {
 movieSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value
 
+    setMovieData()
+
     updateSelectedCount()
 })
 
@@ -30,14 +46,11 @@ movieSelect.addEventListener('change', e => {
 //select or unselect a seat
 container.addEventListener('click', (e) => {
     if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
-        console.log(e.target);
-        e.target.classList.toggle('selected')
 
-        updateSelectedCount()
+        e.target.classList.toggle('selected')
+        console.log(e.target.selectedIndex)
+        updateSelectedCount(e.target.selectedIndex, e.target.value)
     };
 })
 
 
-
-
-console.log(typeof seats);
